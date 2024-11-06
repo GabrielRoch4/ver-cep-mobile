2afd2e3e34e7598ff577dd9a2401647347e026ad
Gabriel Rocha <111160701+GabrielRoch4@users.noreply.github.com>
2024-11-06 10:24:48 -0300

    Front

--- app/src/main/java/com/example/vercep/MainActivity.kt
+++ app/src/main/java/com/example/vercep/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.example.vercep
-
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val spinnerUf = findViewById<Spinner>(R.id.spinner_uf)
+
+        // Configura o adaptador para o Spinner
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.brazilian_states,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerUf.adapter = adapter
+        }
+
+        // Configura um listener para verificar a seleção
+        spinnerUf.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val selectedState = parent.getItemAtPosition(position).toString()
+                if (selectedState == "Selecione um estado") {
+                    // Nenhum estado foi selecionado, mostra uma dica ou faz algo
+                } else {
+                    // Um estado foi selecionado
+                    Toast.makeText(this@MainActivity, "Estado selecionado: $selectedState", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Nenhuma ação necessária aqui
+            }
         }
     }
 }
